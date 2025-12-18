743d11218b56177a65f36aeb53815fa7b32e2817
Pete Savage <psavage@redhat.com>
2025-12-18 17:54:51 +0000

    Trying a new idea

--- image2/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Hello from Image 2!"
-echo "This container proves the signed/sealed image chain."
-echo "Image 1 Digest: ${IMAGE1_DIGEST}"
-echo "Image 1 URL: ${IMAGE1_URL}"
-echo "This digest was baked in at build time, proving atomic build."
-echo "Timestamp: $(date)"
